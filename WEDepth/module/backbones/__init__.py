d348caee8b20bc7a4cc6f4fdb2d6474ee5e71d3e
Book-W <37619866+book-book24@users.noreply.github.com>
2026-01-29 16:55:50 +0800

    Delete WEDepth directory

--- WEDepth/module/backbones/__init__.py
+++
@@ -1,4 +0,0 @@
-from .resnet import Bottleneck, ResNet, _resnet
-from .swin import SwinTransformer
-
-__all__ = ["_resnet", "ResNet", "Bottleneck", "SwinTransformer"]
--- WEDepth/train.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 -m torch.distributed.launch --nproc_per_node=4 train.py --config-file "./configs/nyu.json"
